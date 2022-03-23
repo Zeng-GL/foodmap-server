2815c24d3c5a13b4992355999c1a32d40bd79e78
Zeng-GL <aqua62786@gmail.com>
2022-03-24 07:20:19 +0800

    edit_and_delete_function

--- routes/restaurant.js
+++ routes/restaurant.js
@@ -28,4 +28,35 @@ router.post("/", async (req, res) => {
   }
 });
 
+router.patch("/:restId",async(req,res)=>{
+  try{
+    const editedRest = await Restaurant.updateOne(
+      {_id:req.params.restId},
+      {
+        $set:{
+          name: req.body.name,
+          lat: req.body.lat,
+          lng: req.body.lng,
+          location: req.body.location,
+          food: req.body.food,
+        },
+      }
+    );
+    res.json(editedRest)
+  }catch (e) {
+    res.json({ message: e });
+  }
+});
+
+router.delete("/:restId",async(req,res)=>{
+  try{
+    const removedRest = await Restaurant.remove({
+      _id: req.params.restId,
+    });
+    res.json(removedRest)
+  }catch (e) {
+    res.json({ message: e });
+  }   
+});
+
 module.exports = router;
